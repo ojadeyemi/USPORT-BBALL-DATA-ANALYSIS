4121d789d1bf3cc9c0058429483f62857571fa3d
OJ Adeyemi <bukunmiojie@gmail.com>
2024-03-19 00:18:41 -0400

    testing sportypy library

--- testsporty.py
+++ testsporty.py
@@ -2,6 +2,13 @@
 from sportypy.surfaces.basketball import NBACourt
 import matplotlib.pyplot as plt
 
-nba = NBACourt( ).draw()
-plt.savefig('BallCourt2', dpi=300)
+nba = NBACourt(color_updates={
+            "plot_background": "#000000",
+            "defensive_half_court": "#ffffff",
+            "offensive_half_court": "#008fff",
+            "court_apron": "#098888",
+            "center_circle_outline": "#000000",
+            "center_circle_fill": "#ffffff"}).draw()
+#plt.savefig('BallCourt2', dpi=300)
+plt.show()
 
